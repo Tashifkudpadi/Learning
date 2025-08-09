65fb41eded8e80bd2ce3864c58d89c6d74490c1c
Tashif Madani <tashif.madani@thetadirect.com>
2025-08-09 13:30:05 +0530

    search query param

--- python/fastAPI/app/main.py
+++ python/fastAPI/app/main.py
@@ -1,7 +1,4 @@
-import time
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from .database import engine
 from . import models
 from .routers import post, user, auth
@@ -11,18 +8,6 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
-while True:
-    try:
-        connection = psycopg2.connect(host="localhost", database="fastapi",
-                                      user="postgres", password="Theta$999", cursor_factory=RealDictCursor)
-        cursor = connection.cursor()
-        print("Database connection successful")
-        break
-    except Exception as error:
-        print("Failed to connect to database")
-        print("Error: ", error)
-        time.sleep(2)
 
 app.include_router(post.router)
 app.include_router(user.router)
