ebffd9b76309d24fa012e009f7b251a4296190c5
Tashif Madani <tashif.madani@thetadirect.com>
2025-08-09 15:06:44 +0530

    migrated to environmental variables

--- python/fastAPI/app/oauth2.py
+++ python/fastAPI/app/oauth2.py
@@ -8,15 +8,16 @@ from fastapi import HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from .database import get_db
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 # command to generate the secret key
 # openssl rand - hex 32
-SECRET_KEY = "e7c90f2ceb97fd95c8cd84ece59521fa46f8c954bf10236d2bc45a6ef75f83cf"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_IN_MINUTES = 60 * 24 * 7
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_IN_MINUTES = settings.access_token_expire_minutes
 
 
 # def create_access_token(data: dict):
